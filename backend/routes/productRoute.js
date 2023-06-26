23d53d98d2ac5425ceb1dd2fc376519e57bae6be
Priti Khodke <72237736+pritikhodke22@users.noreply.github.com>
2023-06-26 23:00:53 +0530

    backend files

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const {
+  getAllProducts,
+  createProduct,
+  updataProduct,
+  deleteProduct,
+  getAllProductDetails,
+  createProductReview,
+  getProductReviews,
+  deleteReview,
+} = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts);
+router
+  .route("/admin/product/new")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), createProduct);
+router
+  .route("/admin/product/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updataProduct)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(getAllProductDetails);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+router
+  .route("/reviews")
+  .get(getProductReviews)
+  .delete(isAuthenticatedUser, deleteReview);
+module.exports = router;
